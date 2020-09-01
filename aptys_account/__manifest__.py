682938c20f126e62d34f48447a6a86a20ff33242
Benjamin Dugardin <bdu@idealisconsulting.com>
2020-09-01 11:51:15 +0200

    [ADD] add module for custom innvoice

---
+++ aptys_account/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Aptys account',
+    'version': '13.0.1.0.0',
+    'summary': """Additional features for account module""",
+    'description': 'Additional features for account odoo module',
+    'category': 'Account',
+    'author': 'Idealis Consulting',
+    'website': 'http://www.idealisconsulting.com',
+    'depends': ['base','account'],
+    'data': [
+        'report/invoice_report_templates.xml',
+    ],
+    'images': [],
+    'license': 'LGPL-3',
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
